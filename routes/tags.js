e0ee1708e535e563d537f98d7e1b217e31bd0e79
nrlysek <nrlysek@users.noreply.github.com>
2017-07-10 22:45:18 -0700

    Removed search by slug, replaced
    Replaced query tags by slug and replaced by query tags by name.

--- routes/tags.js
+++ routes/tags.js
@@ -4,7 +4,7 @@ var router = express.Router();
 var tagUtility = require('../utilities/tags');
 
 router.get('/tags/',function (req, res) {
-    tagUtility.getTags(req.query.id, req.query.minRequestRank, req.query.minAssignRank, req.query.parentId, req.query.visible, req.query.slug).then ((tagObjectArray) => {
+    tagUtility.getTags(req.query.id, req.query.minRequestRank, req.query.minAssignRank, req.query.parentId, req.query.visible, req.query.name).then ((tagObjectArray) => {
         res.json(tagObjectArray);
         res.end();
     });
--- utilities/tags.js
+++ utilities/tags.js
@@ -5,7 +5,7 @@ var announcementsUtility = require('./announcements');
 // GETTERS
 // Gets tags
 
-exports.getTags = function(id, minRequestRank, minAssignRank, parentId, visibility, slug) {
+exports.getTags = function(id, minRequestRank, minAssignRank, parentId, visibility, name) {
     var statement = 'SELECT * FROM tags';
     var statementParameters = {};
 
@@ -14,7 +14,7 @@ exports.getTags = function(id, minRequestRank, minAssignRank, parentId, visibili
     if(typeof minRequestRank != 'undefined') { statementParameters.minAssignRank = minAssignRank; };
     if(typeof parentId != 'undefined') { statementParameters.parentId = parentId; };
     if(typeof visibility != 'undefined') { statementParameters.visibility = visibility; }
-    if(typeof slug != 'undefined') { statementParameters.slug = slug; }
+    if(typeof name != 'undefined') { statementParameters.name = name; }
 
     if(Object.keys(statementParameters).length != 0) {
         statement += ' WHERE ';
