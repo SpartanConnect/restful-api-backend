83891a6249fe7d8a251e3f88f3c402d06d4f954f
nrlysek <nrlysek@users.noreply.github.com>
2017-07-12 11:14:55 -0700

    Removed tag fetch console log
    Cleans up node.js console.

--- utilities/tags.js
+++ utilities/tags.js
@@ -24,7 +24,7 @@ exports.getTags = function(id, minRequestRank, minAssignRank, parentId, visibili
         });
     }
 
-    console.log(statement);
+    //console.log(statement);
 
     return database.query(statement, statementParameters);
 }
