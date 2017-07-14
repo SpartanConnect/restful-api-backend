24a8b6c1938b409ee79dadcf815405e3c117253b
encadyma <encadyma@gmail.com>
2017-07-14 15:20:02 -0700

    We can authenticate people now \!

--- utilities/auth.js
+++ utilities/auth.js
@@ -25,18 +25,94 @@
 var google = require('googleapis');
 var OAuth2 = google.auth.OAuth2;
 
+var database = require('./database');
+
 var authClient = new OAuth2(
     process.env.GOOGLE_CLIENT_ID,
     process.env.GOOGLE_CLIENT_SECRET,
     process.env.GOOGLE_CLIENT_REDIRECT_URL
 );
 
+// How we will retrieve profile information
+var authGetter = google.oauth2({
+    auth: authClient,
+    version: 'v2'
+})
+
 exports.generateConsentUrl = function() {
-    
-    return authClient.generateConsentUrl({
+    return authClient.generateAuthUrl({
         access_type: 'offline',
-        scope: []                       // Google scopes
+        scope: ['https://www.googleapis.com/auth/userinfo.profile', 'https://www.googleapis.com/auth/userinfo.email']               // Google scopes
     });
+}
+
+exports.validateAuthCode = function(code, callback) {
+    authClient.getToken(code, (err, tokens) => {
+        callback(err, tokens);
+    });
+}
+
+exports.generateAuthResponse = function(success, message, data = null) {
+    return {success: success, message: message, data: data};
+}
+
+exports.checkIfUserExists = function(access_token, refresh_token, callback) {
+    authClient.setCredentials({
+        access_token: access_token,
+        refresh_token: refresh_token
+    });
+    authGetter.userinfo.v2.me.get((err, res) => {
+        if (err) { callback(false, null) }
+        else {
+            database.query("SELECT * FROM users WHERE gid = :gid OR email = :email", {
+                gid: res.id,
+                email: res.email
+            }).then((data) => {
+                callback(true, (data.length === 1 ? true : false), false, res);
+            });
+        }
+    });
+}
+
+exports.generateFilledUser = function(gid, name, email, profileUrl, callback) {
+    database.query(
+        "INSERT INTO users (name, email, rank, lastLogin, gid, profileUrl) VALUES (:name, :email, 4, CURRENT_TIMESTAMP, :gid, :profileUrl)", {
+        name: name,
+        email: email,
+        gid: gid,
+        profileUrl: profileUrl
+    }).then((data) => {
+        callback(data.affectedRows);
+    });
+}
+
+exports.updateUsertoFilled = function(gid, name, email, profileUrl, callback) {
+    // Make filled by email (not gid)
+    database.query(
+        "UPDATE users SET name = :name, gid = :gid, profileUrl = :profileUrl, lastLogin = CURRENT_TIMESTAMP WHERE email = :email", {
+        name: name,
+        email: email,
+        gid: gid,
+        profileUrl: profileUrl
+    }).then((data) => {
+        callback(data.affectedRows);
+    });
+}
+
+exports.loginUser = function(gid, email, profileUrl, callback) {
+    database.query(
+        "UPDATE users SET profileUrl = :profileUrl, lastLogin = CURRENT_TIMESTAMP, email = :email WHERE gid = :gid", {
+        gid: gid,
+        email: email,
+        profileUrl: profileUrl
+    }).then((data) => {
+        callback(data.affectedRows);
+    });
+}
+
+exports.setLoginCookie = function(access_token, refresh_token, callback) {
+    // rip
+    callback(true);
 }
 
 // Utilize exports instead of module.exports every time
